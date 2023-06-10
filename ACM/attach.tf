c3bf9a91e1a7425ac4e14f7960164de9cd378b94
Abhijeet Nikam <35236714+abhijeetnikam1995@users.noreply.github.com>
2023-06-10 11:22:42 +0530

    Add files via upload

--- ACM/attach.tf
+++ ACM/attach.tf
@@ -1,5 +1,5 @@
 resource "aws_lb_listener_certificate" "example" {
 #certificate_arn = var.cert_arn
 certificate_arn = data.aws_acm_certificate.amazon_issued.arn
-listener_arn = var.listener_arn
+listener_arn = data.aws_lb_listener.selected443.arn 
 }
--- ACM/cert_arn_datasource.tf
+++ ACM/cert_arn_datasource.tf
@@ -10,3 +10,25 @@ output "cert_arn" {
   value = data.aws_acm_certificate.amazon_issued.arn
 }
 
+data "aws_lb_listener" "selected443" {
+  load_balancer_arn = data.aws_lb.selected.arn
+#  load_balancer_arn = "arn:aws:elasticloadbalancing:us-east-1:502447419870:loadbalancer/app/Web-QA-alb/d2719e095ca4ae2a"
+  port              = 443
+}
+
+output "listener_arn" {
+  description = " ARN of listener"
+  value = data.aws_lb_listener.selected443.arn
+}
+
+
+data "aws_lb" "selected" {
+  name = "Web-QA-alb"
+}
+
+output "ll_arn" {
+  description = " ARN of ALB"
+  value = data.aws_lb.selected.arn
+}
+
+
